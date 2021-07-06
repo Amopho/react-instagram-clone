d7de4f47ca267329167625c25da452cf99de4aa4
Amopho <amopho78@gmail.com>
2021-07-06 15:07:09 +0200

    creating data json

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,18 @@
+import Data from "../data.json";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState } from "react";
+
 const Photo = () => {
-  return <h1>Coming Soon</h1>;
+  const [data, setData] = useState(Data);
+  console.log(process.env);
+  return (
+    <Router>
+      <img
+        src={`${process.env.PUBLIC_URL}/${data[2].img}`}
+        alt="Pink glasses"
+      />
+    </Router>
+  );
 };
 
 export default Photo;
